eed4a15987f7d38f979c38bdff5bc2a4ff6b114f
Boyko Igor <igosja@ukr.net>
2017-07-20 16:19:24 +0300

    +

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -18,12 +18,12 @@ class AppAsset extends AssetBundle
         '//cdn.jsdelivr.net/jquery.slick/1.6.0/slick.css',
     ];
     public $js = [
+        '//maps.googleapis.com/maps/api/js?key=AIzaSyAYBg8KC7jzGXqsJO4ZvBUBr-zHT_0qm2s&callback=initMap',
         'js/vendor/modernizr-2.6.2-respond-1.1.0.min.js',
         'js/vendor/libs.js',
         'js/main.js',
         'js/site.js',
         '//cdn.jsdelivr.net/jquery.slick/1.6.0/slick.min.js',
-        '//maps.googleapis.com/maps/api/js?key=AIzaSyAYBg8KC7jzGXqsJO4ZvBUBr-zHT_0qm2s&callback=initMap',
     ];
     public $depends = [
         'yii\web\YiiAsset',
